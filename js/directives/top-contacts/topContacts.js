c7a404e41538eb1f7832c8b1583dd660e09ef3e8
Rubin Njagi <rubinnjagi@yahoo.com>
2017-04-07 13:13:52 +0300

    changed path cases to solve missing directives

--- js/directives/top-contacts/topContacts.js
+++ js/directives/top-contacts/topContacts.js
@@ -9,7 +9,7 @@ app.directive('topContacts', function(){
 		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
 		// template: '',
-		templateUrl: 'js/directives/topcontacts/topcontacts.html',
+		templateUrl: 'js/directives/top-contacts/topContacts.html',
 		// replace: true,
 		// transclude: true,
 		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
